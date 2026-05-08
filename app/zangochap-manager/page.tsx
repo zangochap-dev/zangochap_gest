e1c3369199fec9cfbbcfa606fcdee05a1836d130
moasko.dev <42101671+moasko@users.noreply.github.com>
2026-05-08 03:52:13 +0000

    Update page.tsx

--- app/zangochap-manager/page.tsx
+++ app/zangochap-manager/page.tsx
@@ -2,6 +2,8 @@ import { getSession } from "@/modules/auth/actions";
 import { redirect } from "next/navigation";
 import LoginClient from "./LoginClient";
 
+export const dynamic = "force-dynamic";
+
 export default async function LoginPage() {
   const session = await getSession();
 

53a853c480ce35942e46ffb654773f0ecfbc4bc2
moasko.dev <42101671+moasko@users.noreply.github.com>
2026-05-08 04:35:29 +0000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,20 @@ const nextConfig: NextConfig = {
   output: 'standalone',
   experimental: {
     serverActions: {
-      allowedOrigins: ["172.20.10.2", "localhost:3000", "russel-unclinging-incalculably.ngrok-free.dev"],
+      allowedOrigins: [
+        "172.20.10.2", 
+        "localhost:3000", 
+        "russel-unclinging-incalculably.ngrok-free.dev",
+        "*.traefik.me",
+        "72-62-45-245.traefik.me"
+      ],
     },
   },
-  allowedDevOrigins: ["russel-unclinging-incalculably.ngrok-free.dev", "172.20.10.2"],
+  allowedDevOrigins: [
+    "russel-unclinging-incalculably.ngrok-free.dev", 
+    "172.20.10.2",
+    "*.traefik.me"
+  ],
 };
 
 export default nextConfig;

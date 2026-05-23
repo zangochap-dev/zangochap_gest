d7e048e6ee09664bae5ed9dd98b94837a534caca
moasko.dev <42101671+moasko@users.noreply.github.com>
2026-05-23 14:58:02 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -105,18 +105,28 @@ export default async function RootLayout({
       <body suppressHydrationWarning>
         {hasGoogleAnalytics && (
           <>
-            <Script
-              src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`}
-              strategy="afterInteractive"
-            />
-            <Script id="google-analytics" strategy="afterInteractive">
+            {/* Google Tag Manager (noscript) */}
+            <noscript>
+              <iframe 
+                src={`https://www.googletagmanager.com/ns.html?id=${googleAnalyticsId}`}
+                height="0" 
+                width="0" 
+                style={{ display: "none", visibility: "hidden" }}
+              />
+            </noscript>
+            {/* End Google Tag Manager (noscript) */}
+            
+            {/* Google Tag Manager */}
+            <Script id="google-tag-manager" strategy="afterInteractive">
               {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${googleAnalyticsId}');
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${googleAnalyticsId}');
               `}
             </Script>
+            {/* End Google Tag Manager */}
           </>
         )}
         {hasFacebookPixel && (
